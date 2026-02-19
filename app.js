ca3404bba1b5f8e26c55732ccd2be612099a04c3
buregeyaleonard1 <buregeyaleonard1@gmail.com>
2026-02-19 18:39:26 +0200

    Add function to show specific section

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
-// None functions zose za menu zizokora hano
-// Ubu twakoresheje alert gusa kugirango turebe ico click ikora
+function showSection(sectionId){
+  const sections = document.querySelectorAll('.section');
+  sections.forEach(s => s.classList.remove('active'));
+  document.getElementById(sectionId).classList.add('active');
+}
+
+// Default: show Home
+showSection('home');
